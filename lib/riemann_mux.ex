dce3b6bca99d02289ecb5cae543bd918e46ff733
Fernando Alonso <krakatoa1987@gmail.com>
2017-07-04 10:06:25 -0300

    Connect as a WS client

--- lib/riemann_mux.ex
+++ lib/riemann_mux.ex
@@ -15,4 +15,26 @@ defmodule RiemannMux do
   def hello do
     :world
   end
+
+  def spin do
+    connect
+    loop
+  end
+
+  def connect do
+    {:ok, pid} = :gun.open('127.0.0.1', 5556, %{:retry => 0})
+    {:ok, :http} = :gun.await_up(pid)
+    :gun.ws_upgrade(pid, "/index?subscribe=true&query=true", [], %{:compress => true})
+    receive do
+      {:gun_ws_upgrade, pid, :ok, _headers} -> :ok
+    end
+  end
+
+  def loop do
+    receive do
+      {:gun_ws, pid, frame} ->
+        IO.puts(inspect frame)
+        loop
+    end
+  end
 end
